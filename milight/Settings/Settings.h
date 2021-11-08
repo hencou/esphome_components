7cb396673092ec327639587ecd99a6c5d0efb799
hencou <32905287+hencou@users.noreply.github.com>
2021-11-08 11:34:16 +0100

    Update Settings.h

--- milight/Settings/Settings.h
+++ milight/Settings/Settings.h
@@ -123,7 +123,7 @@ public:
     rf24ListenChannel(RF24Channel::RF24_LOW),
     packetRepeatsPerLoop(10),
     wifiMode(WifiMode::N),
-    defaultTransitionPeriod(200),
+    defaultTransitionPeriod(500),
     _autoRestartPeriod(0)
   { }
 
