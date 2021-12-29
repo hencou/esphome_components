7ef09edd592fba30f68e980143de7450d6653914
hencou <32905287+hencou@users.noreply.github.com>
2021-12-29 15:59:13 +0100

    Update mi.h

--- components/mi/mi.h
+++ components/mi/mi.h
@@ -76,6 +76,18 @@ namespace esphome {
         void set_ce_pin(InternalGPIOPin *pin) {settings.cePin = pin->get_pin();}
         void set_csn_pin(InternalGPIOPin *pin) {settings.csnPin = pin->get_pin();}
         void set_reset_pin(InternalGPIOPin *pin) {settings.resetPin = pin->get_pin();}
+        void set_radio_interface_type(String value) {settings.radioInterfaceType = Settings::typeFromString(value);}
+        void set_packet_repeats(size_t value) {settings.packetRepeats = value;}
+        void set_listen_repeats(uint8_t value) {settings.listenRepeats = value;}
+        void set_state_flush_interval(size_t value) {settings.stateFlushInterval = value;}
+        void set_packet_repeat_throttle_threshold(size_t value) {settings.packetRepeatThrottleThreshold = value;}
+        void set_packet_repeat_throttle_sensitivity(size_t value) {settings.packetRepeatThrottleSensitivity = value;}
+        void set_packet_repeat_minimum(size_t value) {settings.packetRepeatMinimum = value;}
+        void set_enable_automatic_mode_switching(bool value) {settings.enableAutomaticModeSwitching = value;}
+        void set_rf24_power_level(String value) {settings.rf24PowerLevel = RF24PowerLevelHelpers::valueFromName(value);}
+        //void set_rf24_channels(String value) {/*TODO: settings.rf24Channels =*/ }
+        void set_rf24_listen_channel(String value) {settings.rf24ListenChannel = RF24ChannelHelpers::valueFromName(value);}
+        void set_packet_repeats_per_loop(size_t value) {settings.packetRepeatsPerLoop = value;}
       private:
 
         Settings settings;
