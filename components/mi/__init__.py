76abcc368f80f1037e326e8e4245f5fcc8bfa970
hencou <32905287+hencou@users.noreply.github.com>
2022-01-12 16:21:53 +0100

    Update __init__.py

--- components/mi/__init__.py
+++ components/mi/__init__.py
@@ -69,11 +69,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    cg.add_library("SPI", None)
-    cg.add_library("RF24", None)
-    cg.add_library("PathVariableHandlers", None)
     cg.add_library("https://github.com/luisllamasbinaburo/Arduino-List", None)
-    cg.add_library("bblanchon/ArduinoJson", None)
     
     ce_pin = await cg.gpio_pin_expression(config[CONF_CE_PIN])
     cg.add(var.set_ce_pin(ce_pin))
