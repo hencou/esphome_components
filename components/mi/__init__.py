69a61510dd3237278d3b81751a8fd5e46de243dd
hencou <32905287+hencou@users.noreply.github.com>
2022-02-26 14:44:28 +0100

    Update __init__.py

--- components/mi/__init__.py
+++ components/mi/__init__.py
@@ -69,6 +69,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
+    cg.add_library("ListLib", None)
+    
     ce_pin = await cg.gpio_pin_expression(config[CONF_CE_PIN])
     cg.add(var.set_ce_pin(ce_pin))
     csn_pin = await cg.gpio_pin_expression(config[CONF_CSN_PIN])
