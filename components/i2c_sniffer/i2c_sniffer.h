98112d7aa89230100cd8f12fa39057bd93913b72
Henk Courtz <32905287+hencou@users.noreply.github.com>
2022-06-05 08:26:04 +0200

    Add files via upload

--- components/i2c_sniffer/i2c_sniffer.h
+++ components/i2c_sniffer/i2c_sniffer.h
@@ -22,6 +22,13 @@ namespace esphome
 
       void set_sda_pin(uint8_t value) {sda_pin = value;}
       void set_scl_pin(uint8_t value) {scl_pin = value;}
+
+    private:
+      bool buf[4096];
+      bool *bufI = &buf[0];
+      const uint32_t PAUSE = 1 << 20;
+
+      uint32_t waitCl(bool expected);
     };
 
   } // namespace i2c_sniffer
