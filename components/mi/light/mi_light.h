2845c75f070a7d1e410c6bf2f54d27e8e8e73259
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2023-03-19 17:27:09 +0100

    Add reverse function real_color_temperature

--- components/mi/light/mi_light.h
+++ components/mi/light/mi_light.h
@@ -26,15 +26,28 @@ namespace esphome {
 
     void set_cold_white_temperature(float cold_white_temperature) { cold_white_temperature_ = cold_white_temperature; }
     void set_warm_white_temperature(float warm_white_temperature) { warm_white_temperature_ = warm_white_temperature; }
-    float mi_color_temperature(float real_color_temperature);
+
+   protected:
+    float mi_color_temperature(float real_color_temperature) {
+      float pct = (real_color_temperature - cold_white_temperature_) / color_temperature_range();
+      return pct * MI_MIREDS_RANGE + MI_MIREDS_MIN;
+    }
+    float real_color_temperature(float mi_color_temperature) {
+      float pct = (mi_color_temperature - MI_MIREDS_MIN) / MI_MIREDS_RANGE;
+      return pct * color_temperature_range() + cold_white_temperature_;
+    }
 
    private:
+    const float MI_MIREDS_MIN = 153;
+    const float MI_MIREDS_MAX = 370;
+    const float MI_MIREDS_RANGE = MI_MIREDS_MAX - MI_MIREDS_MIN;
+    float cold_white_temperature_;
+    float warm_white_temperature_;
+    float color_temperature_range() { return warm_white_temperature_ - cold_white_temperature_; }
+
     BulbId bulbId = {0, 0, REMOTE_TYPE_RGB_CCT};
     Mi *parent_;
     light::LightState *state_{nullptr};
-
-    float cold_white_temperature_;
-    float warm_white_temperature_;
   };
 
   }  // namespace mi
