9828f50d152ff012d7397e8fdb07f234d61c35f7
hencou <32905287+hencou@users.noreply.github.com>
2025-05-08 00:36:23 +0200

    Update __init__.py

--- components/i2c_sniffer/__init__.py
+++ components/i2c_sniffer/__init__.py
@@ -22,8 +22,8 @@ CONFIG_SCHEMA = (
   cv.Schema(
     {
       cv.GenerateID(): cv.declare_id(I2cSniffer),
-      cv.Optional(CONF_SDA, default="SDA"): pin_with_input_and_output_support,
-      cv.Optional(CONF_SCL, default="SCL"): pin_with_input_and_output_support,
+      #cv.Optional(CONF_SDA, default="SDA"): pin_with_input_and_output_support,
+      #cv.Optional(CONF_SCL, default="SCL"): pin_with_input_and_output_support,
     }
   )
   .extend(cv.COMPONENT_SCHEMA)
@@ -33,5 +33,5 @@ async def to_code(config):
   var = cg.new_Pvariable(config[CONF_ID])
   await cg.register_component(var, config)
   
-  cg.add(var.set_sda_pin(config[CONF_SDA]))
-  cg.add(var.set_scl_pin(config[CONF_SCL]))
+  #cg.add(var.set_sda_pin(config[CONF_SDA]))
+  $cg.add(var.set_scl_pin(config[CONF_SCL]))
