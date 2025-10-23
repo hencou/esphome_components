d8e958b822a5f4af2a42bf9158ffbf3dc9a2a982
hencou <32905287+hencou@users.noreply.github.com>
2025-10-23 23:34:42 +0200

    Update __init__.py

--- components/mi/__init__.py
+++ components/mi/__init__.py
@@ -92,7 +92,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    ##cg.add_library("https://github.com/luisllamasbinaburo/Arduino-List", None)
+    cg.add_library("https://github.com/luisllamasbinaburo/Arduino-List", None)
     cg.add_library("nrf24/RF24", None)
     cg.add_library("PathVariableHandlers", None)
     ##cg.add_library("StreamUtils", None)
@@ -151,5 +151,6 @@ async def to_code(config):
       for conf in config.get(CONF_ON_COMMAND_RECEIVED, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [(MiBridgeData, "data")], conf)
+
 
 
