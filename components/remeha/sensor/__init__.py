e8e1a30c0f7a79048befbce82abde2beb8939b35
hencou <32905287+hencou@users.noreply.github.com>
2026-04-16 20:27:29 +0200

    Update __init__.py

--- components/remeha/sensor/__init__.py
+++ components/remeha/sensor/__init__.py
@@ -23,7 +23,6 @@ CONF_STATUS_CODE = "status_code"
 CONF_SUBSTATUS_CODE = "substatus_code"
 CONF_WATER_PRESSURE = "water_pressure"
 CONF_ROOM_TEMPERATURE = "room_temperature"
-CONF_ROOM_SETPOINT = "room_setpoint"
 CONF_LOCKING_MODE = "locking_mode"
 CONF_BLOCKING_MODE = "blocking_mode"
 CONF_ERROR_HISTORY = "error_history"
@@ -107,13 +106,6 @@ CONFIG_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_TEMPERATURE,
             state_class=STATE_CLASS_MEASUREMENT,
             icon="mdi:home-thermometer",
-        ),
-        cv.Optional(CONF_ROOM_SETPOINT): sensor.sensor_schema(
-            unit_of_measurement=UNIT_CELSIUS,
-            accuracy_decimals=2,
-            device_class=DEVICE_CLASS_TEMPERATURE,
-            state_class=STATE_CLASS_MEASUREMENT,
-            icon="mdi:thermostat",
         ),
         cv.Optional(CONF_LOCKING_MODE): sensor.sensor_schema(
             accuracy_decimals=0,
@@ -220,7 +212,6 @@ SDO_POLL_MAP = {
     CONF_APPLIANCE_VARIANT: (0x5037, 0x00),
     CONF_WATER_PRESSURE: (0x501D, 0x00),
     CONF_ROOM_TEMPERATURE: (0x501D, 0x00),
-    CONF_ROOM_SETPOINT: (0x501D, 0x00),
     CONF_RETURN_TEMPERATURE: (0x501D, 0x00),
     CONF_DHW_TEMPERATURE: (0x501D, 0x00),
     CONF_CONTROL_TEMPERATURE: (0x501D, 0x00),
@@ -250,7 +241,6 @@ async def to_code(config):
         CONF_SUBSTATUS_CODE: "set_substatus_code_sensor",
         CONF_WATER_PRESSURE: "set_water_pressure_sensor",
         CONF_ROOM_TEMPERATURE: "set_room_temperature_sensor",
-        CONF_ROOM_SETPOINT: "set_room_setpoint_sensor",
         CONF_LOCKING_MODE: "set_locking_mode_sensor",
         CONF_BLOCKING_MODE: "set_blocking_mode_sensor",
         CONF_ERROR_HISTORY: "set_error_history_sensor",
