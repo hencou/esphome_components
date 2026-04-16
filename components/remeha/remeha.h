ad8446c8f952dc37bcbfabb12e1b788d2fb758a2
hencou <32905287+hencou@users.noreply.github.com>
2026-04-16 20:45:47 +0200

    Update remeha.h

--- components/remeha/remeha.h
+++ components/remeha/remeha.h
@@ -95,14 +95,14 @@ class Remeha : public Component {
   void set_heating_curve_slope_number(number::Number *n) { this->heating_curve_slope_ = n; }
   //void set_ch_max_flow_temperature_number(number::Number *n) { this->ch_max_flow_temperature_ = n; }
   void set_room_sensor_calibration_number(number::Number *n) { this->room_sensor_calibration_ = n; }
-  //void set_anti_legionella_setpoint_number(number::Number *n) { this->anti_legionella_setpoint_ = n; }
+  void set_anti_legionella_setpoint_number(number::Number *n) { this->anti_legionella_setpoint_ = n; }
 #endif
 
 #ifdef USE_SELECT
   void set_zone_mode_select(RemehaSelect *s) { this->zone_mode_ = s; }
   void set_time_program_select(RemehaSelect *s) { this->time_program_ = s; }
   void set_dhw_enabled_select(RemehaSelect *s) { this->dhw_enabled_ = s; }
-  //void set_anti_legionella_mode_select(RemehaSelect *s) { this->anti_legionella_mode_ = s; }
+  void set_anti_legionella_mode_select(RemehaSelect *s) { this->anti_legionella_mode_ = s; }
   void set_fireplace_mode_select(RemehaSelect *s) { this->fireplace_mode_ = s; }
 #endif
 
@@ -223,14 +223,14 @@ class Remeha : public Component {
   number::Number *heating_curve_slope_{nullptr};
   //number::Number *ch_max_flow_temperature_{nullptr};
   number::Number *room_sensor_calibration_{nullptr};
-  //number::Number *anti_legionella_setpoint_{nullptr};
+  number::Number *anti_legionella_setpoint_{nullptr};
 #endif
 
 #ifdef USE_SELECT
   RemehaSelect *zone_mode_{nullptr};
   RemehaSelect *time_program_{nullptr};
   RemehaSelect *dhw_enabled_{nullptr};
-  //RemehaSelect *anti_legionella_mode_{nullptr};
+  RemehaSelect *anti_legionella_mode_{nullptr};
   RemehaSelect *fireplace_mode_{nullptr};
 #endif
 
