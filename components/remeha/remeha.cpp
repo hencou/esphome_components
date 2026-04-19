42ace422ee50bcb2aa255d2ca3409e6af50f4964
hencou <32905287+hencou@users.noreply.github.com>
2026-04-19 19:18:17 +0200

    Update remeha.cpp

--- components/remeha/remeha.cpp
+++ components/remeha/remeha.cpp
@@ -491,10 +491,6 @@ void Remeha::handle_0x1c1_(const std::vector<uint8_t> &x) {
       float temp = (value & 0xFFFF) * 0.01f;
       this->anti_legionella_setpoint_->publish_state(temp);
       ESP_LOGD(TAG, "Anti-legionella setpoint=%.0f C", temp);
-    } else if (index == 0x3620 && sub == 0x00 && this->dhw_boost_start_ != nullptr) {
-      uint8_t val = value & 0xFF;
-      this->dhw_boost_start_->publish_state(val);
-      ESP_LOGD(TAG, "DHW Boost start=%d", val);
     } else
 #endif
 #ifdef USE_SELECT
